65ef88afdeb31b2052e52bb061b5821e15241021
Pedro Nogueira <pedromcn@vault42.net>
2012-10-27 14:01:36 +0200

    Flush now works as expected.

---
+++ inc/mods/test/class_testView.inc.php
@@ -0,0 +1,33 @@
+<?
+class test_view extends modView 
+{
+	var $mod_name = 'test';
+	/**
+	*	view method distribution
+	*/
+	function main($vid, $method_name) 
+	{
+		$this->vid = $vid;
+		switch(strtolower($method_name)) 
+		{
+			case 'test':			
+			default:						$this->test();					break;
+		}
+	}
+	/**
+	*	start here to learn how to work with contrails
+	*/
+	function test()
+	{
+
+		if($this->access('test'))
+		{
+			$this->set_var('link',$this->lnk(array('event' => 'test')));
+		}
+		$f = new forms('test');
+		$this->set_var('form',$f->show());
+		$this->show('test');
+		
+	}
+}
+?>
